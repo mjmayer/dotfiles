7bbed048e9264d77a8dbc080fbcce24161a83d14
Michael Mayer <mjmayer@ucdavis.edu>
2020-05-22 14:19:55 -0700

    Remove recursive flag

--- fetch_tmux_config.sh
+++ fetch_tmux_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -r -q https://raw.githubusercontent.com/mjmayer/dotfiles/master/tmux.conf
-wget -r -q https://raw.githubusercontent.com/mjmayer/dotfiles/master/tmux.remote.conf
+wget -q https://raw.githubusercontent.com/mjmayer/dotfiles/master/tmux.conf
+wget -q https://raw.githubusercontent.com/mjmayer/dotfiles/master/tmux.remote.conf
