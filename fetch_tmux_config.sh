615f1726ab1b70688373cb357db55e3200a3e623
Michael Mayer <mjmayer@ucdavis.edu>
2020-05-22 14:21:18 -0700

    Add filename to overwrite

--- fetch_tmux_config.sh
+++ fetch_tmux_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -q https://raw.githubusercontent.com/mjmayer/dotfiles/master/tmux.conf
-wget -q https://raw.githubusercontent.com/mjmayer/dotfiles/master/tmux.remote.conf
+wget -q https://raw.githubusercontent.com/mjmayer/dotfiles/master/tmux.conf tmux.conf
+wget -q https://raw.githubusercontent.com/mjmayer/dotfiles/master/tmux.remote.conf tmux.remote.conf
