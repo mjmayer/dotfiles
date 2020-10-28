c829b360da078fcac99c9a431ea72b9e280ea93d
Michael Mayer <mjmayer@gmail.com>
2020-10-28 16:24:22 -0700

    Add aws-elbrules alias for viewing load balancer rules

--- workstation_zsh_aliases.zsh
+++ workstation_zsh_aliases.zsh
@@ -1,1 +1,8 @@
 alias gb="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
+
+aws-elbrules() {
+    lbname=${1:-'web-lb-np'}
+    lbarn=$(aws elbv2 describe-load-balancers --query 'LoadBalancers[?LoadBalancerName==`'"$lbname"'`].LoadBalancerArn' --output text)
+    lblistener=$(aws elbv2 describe-listeners --load-balancer-arn $lbarn --query 'Listeners[?Port==`443`].ListenerArn' --output text)
+    aws elbv2 describe-rules --listener-arn $lblistener
+}
