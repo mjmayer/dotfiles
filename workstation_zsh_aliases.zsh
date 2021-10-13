488f5a3e22d19bb3a5eb886d94c2a0ebc970366e
Michael Mayer <mjmayer@ucdavis.edu>
2021-10-13 06:30:22 -0700

    Add alias for uuidgen

--- workstation_zsh_aliases.zsh
+++ workstation_zsh_aliases.zsh
@@ -1,5 +1,7 @@
 alias gb="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 
+alias uuidgen='uuidgen | tr "[:upper:]" "[:lower:]"'
+
 aws-elbrules() {
     lbname=${1:-'web-lb-np'}
     lbarn=$(aws elbv2 describe-load-balancers --query 'LoadBalancers[?LoadBalancerName==`'"$lbname"'`].LoadBalancerArn' --output text)
