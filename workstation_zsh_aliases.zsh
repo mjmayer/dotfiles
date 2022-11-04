518258c633826f4b7b1b632ee55cc00bde030075
Michael Mayer <mjmayer@ucdavis.edu>
2022-11-04 10:55:08 -0700

    Add terraform targets function

--- workstation_zsh_aliases.zsh
+++ workstation_zsh_aliases.zsh
@@ -8,3 +8,11 @@ aws-elbrules() {
     lblistener=$(aws elbv2 describe-listeners --load-balancer-arn $lbarn --query 'Listeners[?Port==`443`].ListenerArn' --output text)
     aws elbv2 describe-rules --listener-arn $lblistener
 }
+
+alias mfa-ait="ykman oath accounts code -s AWS-ait:mjmayer"
+
+# enables a form of wildcard `terraform -target` https://github.com/hashicorp/terraform/issues/2182
+# terraform plan | terraform-targets | grep 'some pattern' | xargs -r terraform apply -auto-approve
+terraform-targets () {
+  sed 's/\x1b\[[0-9;]*m//g' | grep -o '# [^( ]* ' | grep '\.' | sed " s/^# /-target '/; s/ $/'/; "
+}
